0858edbd68f3d1ed4eb699488abd46c9891fa5b2
Joseph Leon <joseph.leon.9@gmail.com>
2016-04-30 20:34:52 -0400

    Add input so the user can get any tickers price from yahoo.

--- yahoo-scraper.rb
+++ yahoo-scraper.rb
@@ -4,11 +4,19 @@ require 'rubygems'
 require 'nokogiri'
 require 'open-uri'
 
+puts "Enter stock ticker"
+ticker = gets.chomp
+
 BASE_YAHOO_URL = "http://finance.yahoo.com"
-LIST_URL = "#{BASE_YAHOO_URL}/q?s=F"
+LIST_URL = "#{BASE_YAHOO_URL}/q?s=#{ticker}"
+puts LIST_URL
 
+stock_ticker_element = '#yfs_184_' + ticker.downcase
+puts stock_ticker_element
 page = Nokogiri::HTML(open(LIST_URL))
-stock_price = page.css('#yfs_l84_f')
+# stock_price = page.css('#yfs_l84_#{ticker}')
+# stock_price = page.css('#yfs_l84_f')
+stock_price = page.css(stock_ticker_element)
 
-puts page
+# puts page
 puts stock_price
