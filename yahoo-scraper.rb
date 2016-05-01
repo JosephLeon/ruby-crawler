62d056b591a227e3eb5b07e2f81b4d81d3f9941b
Joseph Leon <joseph.leon.9@gmail.com>
2016-04-30 20:51:14 -0400

    Testing dynamic selectors as arguments for the css method on the nokogiri onject.

--- yahoo-scraper.rb
+++ yahoo-scraper.rb
@@ -1,5 +1,3 @@
-# http://finance.yahoo.com/q?s=F
-
 require 'rubygems'
 require 'nokogiri'
 require 'open-uri'
@@ -11,12 +9,14 @@ BASE_YAHOO_URL = "http://finance.yahoo.com"
 LIST_URL = "#{BASE_YAHOO_URL}/q?s=#{ticker}"
 puts LIST_URL
 
-stock_ticker_element = '#yfs_184_' + ticker.downcase
+cleaned_ticker = ticker.downcase
+stock_ticker_element = "#yfs_184_#{cleaned_ticker}"
 puts stock_ticker_element
+
 page = Nokogiri::HTML(open(LIST_URL))
-# stock_price = page.css('#yfs_l84_#{ticker}')
-# stock_price = page.css('#yfs_l84_f')
-stock_price = page.css(stock_ticker_element)
+
+stock_price = page.css('#yfs_l84_f')
+# stock_price = page.css(stock_ticker_element)
 
 # puts page
 puts stock_price
