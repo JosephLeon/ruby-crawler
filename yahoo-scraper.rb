7d04dd3080ef2505f0b3ea349b1028a8ca5695df
Joseph Leon <joseph.leon.9@gmail.com>
2016-05-01 10:03:55 -0400

    Iterate over the table1 and then each tr inside to get headers and values. Build that into the stock quote hash.

--- yahoo-scraper.rb
+++ yahoo-scraper.rb
@@ -11,19 +11,22 @@ puts LIST_URL
 
 cleaned_ticker = ticker.downcase
 stock_ticker_element = "#yfs_l84_#{cleaned_ticker}"
-puts stock_ticker_element
+
+stock_quote = {}
 
 page = Nokogiri::HTML(open(LIST_URL))
 stock_price = page.css(stock_ticker_element).text
+stock_quote["stock_price"] = stock_price
 
 # Get data from id table1, contains
 table1 = page.css('#table1')
 table1.each do |row|
-	puts row
+	# puts row
+	row.css('tr').each do |tr|
+		header = tr.css('th').text.gsub!(/[^0-9A-Za-z]/, '').downcase
+		value = tr.css('td').text
+		stock_quote["#{header}"] = value
+	end
 end
 
-stock_quote = {
-	"stock_price" => stock_price,
-}
-
-puts stock_price
+puts stock_quote
