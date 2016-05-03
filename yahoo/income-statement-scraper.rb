561e6fff808e84e31f1e77defc249787d2569045
Joseph Leon <joseph.leon.9@gmail.com>
2016-05-02 22:08:13 -0400

    Test getting revenue.

--- yahoo/income-statement-scraper.rb
+++ yahoo/income-statement-scraper.rb
@@ -7,12 +7,18 @@ ticker = gets.chomp
 
 BASE_YAHOO_URL = "http://finance.yahoo.com"
 LIST_URL = "#{BASE_YAHOO_URL}/q/is?s=#{ticker}+Income+Statement&annual"
-# http://finance.yahoo.com/q/is?s=MSFT+Income+Statement&annual
+MASTER_TABLE_SELECTOR = ".yfnc_tabledata1"
+REVENUE_SELECTOR = ".yfnc_modtitle1"
 
 page = Nokogiri::HTML(open(LIST_URL))
 
 cleaned_ticker = ticker.downcase
 
-financial_data = page.css('.yfnc_tabledata1')
+financial_data = page.css(MASTER_TABLE_SELECTOR)
 
-puts financial_data
+# Revenue
+$revenue_data = {}
+revenue_section = page.css(REVENUE_SELECTOR)
+# $revenue_data["periods"] = revenue_section.css(".yfi-module-title").text
+
+puts revenue_data
